f97906e5ea6676fbf23edf4b05930f9560a3561c
Kiran <rayoflight8904@gmail.com>
2023-08-31 20:20:03 +0530

    first shell

---
+++ 1.hellow World.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "im learning devops"
+
+echo "suresh : good morning ramesh"
+echo "ramesh: hi suresh, good morning"
+echo "ramesh : how ar u doing"
+echo "suresh: iam doing good ramesh"
